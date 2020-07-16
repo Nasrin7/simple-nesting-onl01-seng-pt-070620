34e60c65823b5174b9be07de0f27e8c0149cd883
Nasrin Rahimi <77.nasrin@gmail.com>
2020-07-16 11:24:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,8 +84,10 @@ def adding_matz
         }
      }
      
-     binding.pry
-     programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby"}
+    
+     programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby",languages: ["LISP", "C"]}
+      binding.pry
+     programmer_hash
 
     
 end
