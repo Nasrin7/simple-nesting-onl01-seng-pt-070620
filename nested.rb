57d3bc33ad86663f13959ab03366150cba1be942
Nasrin Rahimi <77.nasrin@gmail.com>
2020-07-16 11:17:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,7 +59,7 @@ def dennis_ritchies_language
         }
      }
      binding.pry
-     programmer_hash[:dennis_ritchie][:languages][0].to_s
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
